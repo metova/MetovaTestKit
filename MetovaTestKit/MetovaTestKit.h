245d0ef234eb7e6451778eac38c6cae32fa311c5
Nick Griffith <nick.griffith@metova.com>
2016-05-06 18:24:41 -0500

    Initial Commit

---
+++ MetovaTestKit/MetovaTestKit.h
@@ -0,0 +1,19 @@
+//
+//  MetovaTestKit.h
+//  MetovaTestKit
+//
+//  Created by Nick Griffith on 5/6/16.
+//  Copyright © 2016 Metova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MetovaTestKit.
+FOUNDATION_EXPORT double MetovaTestKitVersionNumber;
+
+//! Project version string for MetovaTestKit.
+FOUNDATION_EXPORT const unsigned char MetovaTestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetovaTestKit/PublicHeader.h>
+
+
