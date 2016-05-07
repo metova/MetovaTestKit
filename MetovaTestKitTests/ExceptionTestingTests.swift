c0d2633dcca20cd3e76ff1705b64c4b321f84cff
Nick Griffith <nick.griffith@metova.com>
2016-05-07 17:14:07 -0500

    added two tests for exception tests.  Added two skeleton tests with TODO markers... not sure how to test these cases right now...

--- MetovaTestKitTests/ExceptionTestingTests.swift
+++ MetovaTestKitTests/ExceptionTestingTests.swift
@@ -54,4 +54,51 @@ class ExceptionTestingTests: XCTestCase {
         
         XCTAssertNotNil(exception)
     }
+    
+    func testAssertNoExceptionPasses() {
+        
+        var didReachEnd: Bool?
+        
+        MTKAssertNoException(message: "TEST MESSAGE") {
+            didReachEnd = false
+
+            didReachEnd = true
+        }
+        
+        guard let endReached = didReachEnd else {
+            XCTFail("Test block was not executed.")
+            return
+        }
+        
+        XCTAssertTrue(endReached)
+    }
+    
+    func testAssertNoExceptionFails() {
+        // TODO: Find a way to verify that MTKAssertNoException fails.  I know it fails, but the only way I can get it to fail also fails *this* test itself.  How do we verify a test fails without failing the test that's testing it?
+    }
+    
+    func testAssertExceptionPasses() {
+        
+        var didReachEnd: Bool?
+        
+        MTKAssertException(message: "TEST MESSAGE") {
+            didReachEnd = false
+            
+            let arr = NSArray()
+            arr.objectAtIndex(3)
+            
+            didReachEnd = true
+        }
+        
+        guard let endReached = didReachEnd else {
+            XCTFail("Test block was not executed.")
+            return
+        }
+        
+        XCTAssertFalse(endReached)
+    }
+    
+    func testAssertExceptionFails() {
+        // TODO: Find a way to verify that MTKAssertException fails.  I know it fails, but the only way I can get it to fail also fails *this* test itself.  How do we verify a test fails without failing the test that's testing it?
+    }
 }
