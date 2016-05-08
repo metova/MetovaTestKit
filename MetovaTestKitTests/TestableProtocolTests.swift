47fb27b562e56dd777ab0c2249586f7c574cec4a
Nick Griffith <nick.griffith@metova.com>
2016-05-07 19:07:39 -0500

    added tests for MTKTestable protocol extension

---
+++ MetovaTestKitTests/TestableProtocolTests.swift
@@ -0,0 +1,39 @@
+//
+//  TestableProtocolTests.swift
+//  MetovaTestKit
+//
+//  Created by Nick Griffith on 5/7/16.
+//  Copyright © 2016 Metova. All rights reserved.
+//
+
+import XCTest
+
+class TestableProtocolTests: XCTestCase {
+    
+    func testDidCallLoadView() {
+        let expectation = expectationWithDescription("Block executed")
+        
+        TestableViewController.test { testVC in
+            defer { expectation.fulfill() }
+            
+            XCTAssertNotNil(testVC.view)
+            XCTAssertNotNil(testVC.testLabel)
+            XCTAssertNotNil(testVC.testButton)
+        }
+        
+        waitForExpectationsWithTimeout(0, handler: nil)
+    }
+    
+    func testDidCallViewDidLoad() {
+        let expectation = expectationWithDescription("Block executed")
+        
+        TestableViewController.test { testVC in
+            defer { expectation.fulfill() }
+            
+            XCTAssertEqual(testVC.testLabel.text, TestableViewController.TestLabelString)
+            XCTAssertEqual(testVC.testButton.currentTitle, TestableViewController.TestButtonString)
+        }
+        
+        waitForExpectationsWithTimeout(0, handler: nil)
+    }
+}
