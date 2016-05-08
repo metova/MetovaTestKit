06e1560098379e2852688a374ce957e691caf078
Nick Griffith <nick.griffith@metova.com>
2016-05-08 09:38:42 -0500

    adding unit tests to cover the `MTKAssertNoException` and `MTKAssertException` when default argument for message parameter is used.

--- MetovaTestKitTests/ExceptionTestingTests.swift
+++ MetovaTestKitTests/ExceptionTestingTests.swift
@@ -73,8 +73,30 @@ class ExceptionTestingTests: XCTestCase {
         XCTAssertTrue(endReached)
     }
     
+    func testAssertNoExceptionDefaultMessagePasses() {
+        
+        var didReachEnd: Bool?
+        
+        MTKAssertNoException {
+            didReachEnd = false
+            
+            didReachEnd = true
+        }
+        
+        guard let endReached = didReachEnd else {
+            XCTFail("Test block was not executed.")
+            return
+        }
+        
+        XCTAssertTrue(endReached)
+    }
+    
     func testAssertNoExceptionFails() {
         // TODO: Find a way to verify that MTKAssertNoException fails.  I know it fails, but the only way I can get it to fail also fails *this* test itself.  How do we verify a test fails without failing the test that's testing it?
+    }
+    
+    func testAssertNoExceptionDefaultMessageFails() {
+        // TODO: Same as above method, but use default for message argument.
     }
     
     func testAssertExceptionPasses() {
@@ -98,7 +120,32 @@ class ExceptionTestingTests: XCTestCase {
         XCTAssertFalse(endReached)
     }
     
+    func testAssertExceptionDefaultMessagePasses() {
+        
+        var didReachEnd: Bool?
+        
+        MTKAssertException {
+            didReachEnd = false
+            
+            let arr = NSArray()
+            arr.objectAtIndex(3)
+            
+            didReachEnd = true
+        }
+        
+        guard let endReached = didReachEnd else {
+            XCTFail("Test block was not executed.")
+            return
+        }
+        
+        XCTAssertFalse(endReached)
+    }
+    
     func testAssertExceptionFails() {
         // TODO: Find a way to verify that MTKAssertException fails.  I know it fails, but the only way I can get it to fail also fails *this* test itself.  How do we verify a test fails without failing the test that's testing it?
+    }
+    
+    func testAssertExceptionDefaultMessageFails() {
+        // TODO: Same as above method, but use default for message argument.
     }
 }
