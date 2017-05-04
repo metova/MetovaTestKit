4eef45fb329dd8d8f5cb1ff802efd744b76cb01b
Logan Gauthier <logan.gauthier@metova.com>
2017-05-04 15:42:02 -0500

    Add segmented control titles assertion.

---
+++ MetovaTestKit/UIKitTesting/XCTestCase+UISegmentedControl.swift
@@ -0,0 +1,43 @@
+//
+//  XCTestCase+UISegmentedControl.swift
+//  MetovaTestKit
+//
+//  Created by Logan Gauthier on 5/2/17.
+//  Copyright © 2017 Metova. All rights reserved.
+//
+//  MIT License
+//
+//  Permission is hereby granted, free of charge, to any person obtaining
+//  a copy of this software and associated documentation files (the
+//  "Software"), to deal in the Software without restriction, including
+//  without limitation the rights to use, copy, modify, merge, publish,
+//  distribute, sublicense, and/or sell copies of the Software, and to
+//  permit persons to whom the Software is furnished to do so, subject to
+//  the following conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+//  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+//  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+//  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+import XCTest
+
+extension XCTestCase {
+    
+    public func MTKAssertSegmentedControl(_ segmentedControl: UISegmentedControl, hasSegmentTitles expectedTitles: [String], _ failureMessage: @autoclosure () -> String? = nil, file: String = #file, line: UInt = #line) {
+        
+        let actualTitles = Array(0..<segmentedControl.numberOfSegments).flatMap { segmentedControl.titleForSegment(at: $0) }
+        
+        guard actualTitles == expectedTitles else {
+            recordFailure(withMessage: failureMessage(), description: "Expected segmented control to have segment titles: \(expectedTitles.commaSeparatedQuotedList()). Instead found \(actualTitles.commaSeparatedQuotedList()).", inFile: file, atLine: line, expected: true)
+            return
+        }
+    }
+}
