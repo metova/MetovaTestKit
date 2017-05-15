714b3bbb7acdafd4778e03e07d8a0442f286c5b7
Logan Gauthier <logan.gauthier@metova.com>
2017-05-15 16:16:51 -0500

    Move exception and constraint testing assertions into XCTestCase extensions to be consistent with the other assertions.

--- MetovaTestKit/ConstraintTesting/MTKConstraintTester.swift
+++ MetovaTestKit/ConstraintTesting/MTKConstraintTester.swift
@@ -29,22 +29,25 @@
 
 import XCTest
 
-/**
-  Synchronously tests the provided block for broken constraints. If any constraints are broken, the test fails. Otherwise, it passes.
- 
- - parameter message:   The message to log upon test failure.
- - parameter file:      The file the test is called from.
- - parameter line:      The line number the test is called from.
- - parameter testBlock: The block to test.
- 
- - returns: Number of broken constraints during test
- */
-@discardableResult public func MTKAssertNoBrokenConstraints(message: @autoclosure () -> String? = nil, file: StaticString = #file, line: UInt = #line, testBlock: () -> Void) -> UInt {
+extension XCTestCase {
     
-    let brokenConstraintCount = MTKCountBrokenConstraints(testBlock)
-    let message = message() ?? "Found \(brokenConstraintCount) broken constraints while executing test block."
-    
-    XCTAssertEqual(0, brokenConstraintCount, message, file: file, line: line)
-    
-    return brokenConstraintCount
+    /**
+     Synchronously tests the provided block for broken constraints. If any constraints are broken, the test fails. Otherwise, it passes.
+     
+     - parameter message:   The message to log upon test failure.
+     - parameter file:      The file the test is called from.
+     - parameter line:      The line number the test is called from.
+     - parameter testBlock: The block to test.
+     
+     - returns: Number of broken constraints during test
+     */
+    @discardableResult public func MTKAssertNoBrokenConstraints(message: @autoclosure () -> String? = nil, file: StaticString = #file, line: UInt = #line, testBlock: () -> Void) -> UInt {
+        
+        let brokenConstraintCount = MTKCountBrokenConstraints(testBlock)
+        let message = message() ?? "Found \(brokenConstraintCount) broken constraints while executing test block."
+        
+        XCTAssertEqual(0, brokenConstraintCount, message, file: file, line: line)
+        
+        return brokenConstraintCount
+    }    
 }
